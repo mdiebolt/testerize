ec456706482871e8757eec48999bdb137ff4cb93
Matt Diebolt <mdiebolt@gmail.com>
2012-06-08 15:17:30 -0700

    making middleman project

---
+++ config.rb
@@ -0,0 +1,25 @@
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  set :http_prefix, '/testerize'
+end
