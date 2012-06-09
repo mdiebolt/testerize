0693e656ebddfa0b85e8fdf5368750257ead1aca
Matt Diebolt <mdiebolt@gmail.com>
2012-06-09 15:45:03 -0700

    building static assets

--- javascripts/click_handler.js
+++ javascripts/click_handler.js
@@ -1,29 +1,45 @@
 (function() {
 
   $(function() {
-    $(document).on('click', function(e) {
-      if ($(e.currentTarget).is('.email_modal, .email_modal > *')) {
-        return;
-      }
-      $('.email_modal').hide();
-      return $('.overlay').hide();
+    var closeModal, openModal, thanks, toggleModal;
+    openModal = function() {
+      return toggleModal(true);
+    };
+    closeModal = function() {
+      return toggleModal(false);
+    };
+    toggleModal = function(val) {
+      $('.email_modal').toggle(val);
+      return $('.overlay').toggle(val);
+    };
+    thanks = function() {
+      $('.thanks_message').show();
+      return setTimeout(function() {
+        return $('.thanks_message').fadeOut(200);
+      }, 3000);
+    };
+    $('.overlay').on('click', function(e) {
+      return closeModal();
     });
     $('.pricing_option').on('mousedown', function(e) {
-      return $(e.currentTarget).attr('class', 'pricing_option active');
+      $(e.currentTarget).attr('class', 'pricing_option active');
+      return $('.email_modal input').attr('data-price', $(e.currentTarget).data('price'));
     });
     $('.pricing_option').on('mouseup', function(e) {
       $(e.currentTarget).attr('class', 'pricing_option');
-      $('.email_modal').show();
-      return $('.overlay').show();
+      return openModal();
     });
     return $('.track_email').on('click', function(e) {
-      var _ref;
-      if (((_ref = $(e.currentTarget)) != null ? _ref.val().trim() : void 0) === '') {
+      var input;
+      input = $(this).prev();
+      if ((typeof $this !== "undefined" && $this !== null ? $this.val().trim() : void 0) === '') {
         return;
       }
-      trackEmail($('.email').val());
-      trackPrice(0);
-      return $('.email').val('');
+      trackEmail(input.val().trim());
+      trackPrice(input.data('price'));
+      input.val('');
+      closeModal();
+      return thanks();
     });
   });
 
