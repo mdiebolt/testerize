6309dd19b12f07cc30955c84eac4ccddea30f09b
Matt Diebolt <mdiebolt@gmail.com>
2012-06-08 23:02:19 -0700

    building static assets

--- javascripts/all.js
+++ javascripts/all.js
@@ -31,6 +31,35 @@
 
 }).call(this);
 (function() {
+
+  $(function() {
+    $(document).on('click', function(e) {
+      if ($(e.currentTarget).is('.email_modal, .email_modal > *')) {
+        return;
+      }
+      $('.email_modal').hide();
+      return $('.overlay').hide();
+    });
+    $('.pricing_option').on('mousedown', function(e) {
+      return $(e.currentTarget).attr('class', 'pricing_option active');
+    });
+    $('.pricing_option').on('mouseup', function(e) {
+      $(e.currentTarget).attr('class', 'pricing_option');
+      $('.email_modal').show();
+      return $('.overlay').show();
+    });
+    return $('.track_email').on('click', function(e) {
+      if ($(e.currentTarget).trim().val() === '') {
+        return;
+      }
+      trackEmail($('.email').val());
+      trackPrice(0);
+      return $('.email').val('');
+    });
+  });
+
+}).call(this);
+(function() {
   var analyticsToken;
 
   analyticsToken = "UA-32516600-1";
